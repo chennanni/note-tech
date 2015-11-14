d7103919f62145c08a8f6055c04396414b058a66
Chennan <chennancd@gmail.com>
2015-11-14 10:44:05 -0500

    delete all note folders

--- WebService/src/LearnWebServices/Client/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://web.learn.org/")
-package org.learn.web;
--- WebService/src/LearnWebServices/Server/HelloWorldWSPublisher.java
+++
@@ -1,9 +0,0 @@
-package org.learn.web;
-import javax.xml.ws.Endpoint;  
-
-public class HelloWorldWSPublisher {  
-	public static void main(String[] args) {  
-		Endpoint.publish("http://localhost:2345/WS/HelloWorld",new HelloWorldImpl());
-		System.out.println("Published");
-	}
-}
