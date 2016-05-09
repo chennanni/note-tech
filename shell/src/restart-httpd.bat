a6617f26800842a29dbf028d6bf3463ca741d6f1
Chennan <chennancd@gmail.com>
2016-05-09 14:44:20 -0400

    Create restart-httpd.bat

---
+++ shell/src/restart-httpd.bat
@@ -0,0 +1,22 @@
+What does this script do: it restart the httpd web server.
+
+```
+echo restart apache httpd
+
+@echo off
+echo off
+
+SET @mypath=D:\Program Files\Apache Group\Apache2\bin
+
+SET PATH=%PATH%;%@mypath%
+
+REM echo %@mypath%
+REM PATH
+
+apache -k restart
+```
+
+http://ss64.com/nt/
+https://www.apachelounge.com/viewtopic.php?t=2409
+https://httpd.apache.org/docs/current/platform/windows.html
+http://www.thewindowsclub.com/how-to-schedule-batch-file-run-automatically-windows-7
